1f87a98d44653404af48c3a2babf940dfb393fdb
Carlos Lopez Ramirez <carlosflr12@gmail.com>
2022-02-15 11:55:01 -0500

    Set up app skeleton

---
+++ corsair/corsairDirectory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CorsairdirectoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'corsairDirectory'
---
+++ corsair/corsairProfile/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CorsairprofileConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'corsairProfile'
