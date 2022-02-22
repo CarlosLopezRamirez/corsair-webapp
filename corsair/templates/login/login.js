1b9d17fd8034d47ae2bfe185457c966d787db868
Hays Burns <hays.0.burns@gmail.com>
2022-02-22 18:04:41 -0500

    first draft of login page

---
+++ corsair/templates/login/login.js
@@ -0,0 +1,10 @@
+function validateSignIn() {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const remember = document.getElementById("remember");
+
+
+    console.log(username.value);
+    console.log(password.value);
+    console.log(remember.value);
+}
