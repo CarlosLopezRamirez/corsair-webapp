328f0cad049dcb3301726d63e274b023fe192db1
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2022-03-23 18:21:34 -0400

    add register page

--- corsair/register/urls.py
+++ corsair/register/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'register'
 urlpatterns = [
     path('', views.register, name='register'),
+    path('store-data', views.storeData, name='dbConnetc'),
     path('confirm-email/<token>', views.confirmEmail, name='confirmEmail'),
     path('confirm-email/', views.resendEmail, name='resendEmail')
 ]
--- corsair/register/views.py
+++ corsair/register/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 def register(request):
-    return render(request, 'register/register.html')
+
+    if request.method == "GET":
+        return render(request, 'register/register.html')
+
+
+def storeData(request):
+
+    if request.method == "POST":
+        return render(request, 'corsairDirectory/corsair-directory.html')
+
 
 def confirmEmail(request, token):
     return render(request, 'register/confirm-email.html')
+
 
 def resendEmail(request):
     return render(request, 'register/resend-email.html')
