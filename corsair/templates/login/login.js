b4b763dd81f0bb2e1659d431f5731c118aee3803
Hays Burns <hays.0.burns@gmail.com>
2022-04-03 21:10:17 -0400

    style change

--- corsair/templates/login/login.js
+++ corsair/templates/login/login.js
@@ -9,6 +9,11 @@ function validateSignIn() {
     console.log(remember.value);
 }
 
-function transition() {
-    console.log("yo");
+async function transition() {
+    const input = document.getElementById("input");
+    input.classList.add('animate__animated', 'animate__fadeOut');
+    setTimeout(() => {
+        input.classList.remove('animate__animated', 'animate__fadeOut');
+     }, 400);
+    
 }
--- corsair/templates/login/reset-pw.js
+++ corsair/templates/login/reset-pw.js
@@ -1,5 +1,9 @@
 function reset() {
     const email = document.getElementById("email");
 
-    console.log(email);
+    console.log(email.value);
 }
+
+window.addEventListener('popstate', function() {
+    console.log("o yeaaa");
+});
