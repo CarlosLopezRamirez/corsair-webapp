e8ee121459ef6f3f5d21f267f6034f240ad8c70c
rebeccatian <97220971+rebeccatian@users.noreply.github.com>
2022-04-19 19:04:48 -0400

    created directory.js

---
+++ corsair/main/static/main/js/directory.js
@@ -0,0 +1,58 @@
+var input, input2, filter, members, card, h4, i, txtValue, txtValue1;
+
+function Search(filter) {
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    members = document.getElementById("membersContent")
+    card = members.getElementsByClassName("card");
+
+    //loops through the array of cards
+    for (i = 0; i < card.length; i++) {
+        h4 = card[i].getElementsByTagName('h4')[0];
+        txtValue = h4.innerHTML;
+
+        //checks to see if filter is a substring within txtValue
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          card[i].style.display = "block";
+        } else {
+          card[i].style.display = "none";
+        }
+      }
+}
+
+filterSelection('All');
+function filterSelection(c) {
+
+    members = document.getElementById("membersContent")
+    card = members.getElementsByClassName("card");
+
+    for (i = 0; i < card.length; i++) {
+        p = card[i].getElementsByTagName('p')[0];
+        txtValue1 = p.innerHTML;
+        if (c == 'All') {
+            card[i].style.display = "block";
+        }
+        else {
+            if (txtValue1.indexOf(c) > -1) {
+                card[i].style.display = "block";
+              } else {
+                card[i].style.display = "none";
+              }
+        } 
+      }
+}
+
+function saveMessage() {
+    const title = document.getElementById("title");
+    const content = document.getElementById("content");
+
+    console.log(title.value)
+    console.log(content.value);
+}
+
+
+
+
+
+
+
